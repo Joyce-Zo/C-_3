9f47e62c352e813f483b3944b2aff2c881015065
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2023-02-08 00:03:45 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,7 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
 using namespace std;
-int add(int a, int b);
+void swap1(int a, int b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+void swap2(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void BubbleSort(int* arr,int size)
+{
+	for (int i = 0; i < size-1; i++) // 外层循环，次数-1
+	{
+		for (int j = 0; j < size - i - 1; j++)//内层循环，逐渐减少
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -26,46 +53,26 @@ int main()
 	//p = &b;    // 不行
 	//*p = 100;  // 不行
 
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	cout << arr[0] << endl;
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//cout << arr[0] << endl;
 
-	int* p = arr;       // 数组名即表示首地址
-	cout << *p << endl; // 解引用，打印首地址的数据
+	//int* p = arr;       // 数组名即表示首地址
+	//cout << *p << endl; // 解引用，打印首地址的数据
+	//p++;				// 地址向后移动4个字节，即到第二个数据
 
+	//int num1 = 10;
+	//int num2 = 20;
+	//swap1(num1, num2);
+	//swap2(&num1, &num2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	int arr[10] = { 4, 3, 6, 9, 1, 2, 10 , 8, 7, 5 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < 10; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+	BubbleSort(arr,size);
+	for (int i = 0; i < 10; i++)
+		cout << arr[i] << " ";
 
 	return 0;
 }
